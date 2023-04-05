93b10cedbfd3f47690b18ba0b4bb51bfcf77827b
Dan Stronge <dnlstronge@gmail.com>
2023-04-05 14:19:32 +0100

    [x] - filter functions

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import classes from "./App.module.css";
 
 /* [goal] - Use an API to play around with some data */
 
-function App() {
+const App = () => {
+  
+  const [users, setUsers] = useState([]);
   const [filtered, setFiltered] = useState([]);
-  const [users, setUsers] = useState({});
   const [toggleUsers, setToggleUsers] = useState(false);
   const fetchUsers = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
     console.log(data);
-    setUsers(data);
+    setUsers(data)
+    setFiltered(data);
   };
 
   const buttonHandler = () => {
@@ -22,15 +24,20 @@ function App() {
   };
 
   const handleFilter = (e) => {
+    if(e.target.value.length > 0) {
     let namefilter = e.target.value.toUpperCase();
     let filteredArray = users.filter(
       (user) => user.username.charAt(0) === namefilter
     );
     console.log(filteredArray);
-
+    setFiltered(filteredArray)    
     // setFiltered(users.filter(user => user.name.charAt(0).toLowerCase === ""))
     // console.log(filtered)
-  };
+  } else {
+    console.log(users)
+    setFiltered(users)
+  }
+}
 
   return (
     <div className={classes.app}>
@@ -38,7 +45,7 @@ function App() {
         <h2 className={classes.heading}>API Fetch Practice</h2>
       </header>
       {toggleUsers && (
-        <label className={classes.searchbarLabel} htmlFor="searchinput">
+        <label className={classes.searchbarLabel} htmlFor="searchinput">Filter by username
           <input
             id="searchinput"
             className={classes.searchbar}
@@ -49,9 +56,11 @@ function App() {
         </label>
       )}
       <div className={classes.dataContainer}>
+        {filtered.length < 1 && <p className={classes.userparaError}>No users Found</p>}
         {toggleUsers &&
-          users.length > 0 &&
-          users.map((user) => {
+          filtered.length > 0 &&
+          filtered.map((user) => {
+            
             return (
               <div key={user.id} className={classes.userbox}>
                 <p className={classes.userpara}>Name: {user.name}</p>
@@ -62,7 +71,7 @@ function App() {
           })}
       </div>
       <button onClick={buttonHandler} className={classes.btn}>
-        {toggleUsers ? "SHOW" : "HIDE"}
+        {toggleUsers ? "Hide Users" : "Show Users"}
       </button>
     </div>
   );
