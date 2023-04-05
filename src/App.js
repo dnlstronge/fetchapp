349319ff144a17c2bda7c34a8af10a183a282538
Dan Stronge <dnlstronge@gmail.com>
2023-04-05 10:32:43 +0100

    [x] - set up state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from "react"
 import classes from "./App.module.css"
 
+
+
 function App() {
+
+  const [users, setUsers] = useState({
+    name: "",
+    address: ""
+  })
+
+  
   return (
     <div className={classes.app}>
       <header className={classes.header}>
         <h2 className={classes.heading}>API Fetch Practice</h2>
         <div></div>
       </header>
-
+      <div className={classes.dataContainer}></div>
     </div>
   );
 }
