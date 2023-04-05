6db884a5ff40a7a6eea8dab2cb4e2376aba1f159
Dan Stronge <dnlstronge@gmail.com>
2023-04-05 11:07:43 +0100

    [x] - add css [x] - format

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import React, { useState, useEffect} from "react"
-import classes from "./App.module.css"
-
-
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import classes from "./App.module.css";
 
 /* [goal] - Use an API to play around with some data */
 
 function App() {
-
   const [users, setUsers] = useState({
     name: "",
-    address: ""
-  })
-  const [toggleUsers, setToggleUsers] = useState(false)
-
-
+    address: "",
+  });
+  const [toggleUsers, setToggleUsers] = useState(false);
 
   const buttonHandler = () => {
-    console.log("change handler fired")
-    setToggleUsers(!toggleUsers)
-  }
+    setToggleUsers(!toggleUsers);
+  };
 
   return (
     <div className={classes.app}>
@@ -27,12 +21,13 @@ function App() {
         <h2 className={classes.heading}>API Fetch Practice</h2>
         <div></div>
       </header>
-      
+
       <div className={classes.dataContainer}>
-        {toggleUsers && 
-        <p>I AM A TEST PARAGRAPH</p>}
+        {toggleUsers && <p>I AM A TEST PARAGRAPH</p>}
       </div>
-      <button onClick={buttonHandler} className={classes.btn}>Show Data</button>
+      <button onClick={buttonHandler} className={classes.btn}>
+        Show Data
+      </button>
     </div>
   );
 }
