6a635b4c23fc9cf773b29f0d642e3c2122fec012
Dan Stronge <dnlstronge@gmail.com>
2023-04-06 11:08:31 +0100

    [x] - add error handling for new fetch component

--- src/Components/Dogs/Dogs.js
+++ src/Components/Dogs/Dogs.js
@@ -8,10 +8,35 @@ import useErrorHandler from "../../Hooks/useErrorHandler";
 
 gonna use: https://dog.ceo/dog-api/  */
 
+/* first get an array of rando dogs (/number) */
+
+const dogURL = "https://dog.ceo/dog-api/50"
+
 const Dogs = () => {
+  const [error, setError] = useState({
+    isError: false,
+    status: null,
+    msg: null
+  })      
+  const fetchDogs = async() => {
+    try {
+        const response = await fetch(dogURL)
+        const data = await response.json()
+        if(response?.ok) {
+            setError({isError: false, status: null, msg: null})
+            setDogData(data)
+        } else {
+            setError({isError: true, status: response.status, msg: `Woofs....there has been a ${response.status} error.`})
+        }
+    } catch (error) {
+        setError({isError: true, status: error.status, msg: error.message})
+    }
+  }
   const [dogData, setDogData] = useState([]);
 
-  return <div className={classes.container}></div>;
+  return <div className={classes.container}>
+    <ErrorComp status={error.status} msg={error.msg} />
+  </div>;
 };
 
 export default Dogs;
