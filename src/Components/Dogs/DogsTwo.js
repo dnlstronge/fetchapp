5714390ef42296a1fc5ae78c75a26d7d592341c8
Dan Stronge <dnlstronge@gmail.com>
2023-04-07 10:31:00 +0100

    [x] - css changes

--- src/Components/Dogs/DogsTwo.js
+++ src/Components/Dogs/DogsTwo.js
@@ -1,15 +1,15 @@
 import React from "react";
-import classes from "./Dogs.module.css"
+import classes from "./DogsTwo.module.css"
 import Button from "../../UI/Button";
 
 const DogsTwo = () => {
     return (
         
         <div className={classes.container}>
-            <h2 className={classes.heading}>Dogs using custom hooks
+            <h4 className={classes.heading}>Dogs using custom hooks
             <p className={classes.para}>I've built this section to try out a custom hook I created
             to handle simple fetch requests and errors which fall outside of the fetch API. Upon button press
-            It should return some dog images from the previously used free api.</p></h2>
+            It should return some dog images from the previously used free api.</p></h4>
              <Button text="Show me dogs"></Button>
         </div>
        
