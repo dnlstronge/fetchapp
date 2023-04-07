edc61c620ec933c9ce895489db08f22e14918bfa
Dan Stronge <dnlstronge@gmail.com>
2023-04-07 09:00:56 +0100

    [x] - format

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,5 +1,5 @@
-import useErrorHandler from "./useErrorHandler"
-import React, { useState } from "react"
+import useErrorHandler from "./useErrorHandler";
+import React, { useState } from "react";
 
 /* Custom fetch hook. 
 
@@ -9,41 +9,45 @@ why not have just one object?
 
 */
 
-const useFetch = async(url) => {
-    const [response, setResponse] = useState({
-        status: null,
-        isLoading: false,
-        error: {isError: false, msg: null}, 
-        data: []
+const useFetch = async (url) => {
+  const [response, setResponse] = useState({
+    status: null,
+    isLoading: false,
+    error: { isError: false, msg: null },
+    data: [],
+  });
 
-    })
-  
-    try {
-        const res = await fetch(url)
-        const data = await res.json()
-        if(res?.ok) {
-            setResponse({
-                status: res.status,
-                isLoading: false,
-                error: {isError: false, msg: null},
-                data: data
-            })
-        } else {
-            setResponse({
-                ...response, 
-                isLoading: false, 
-                status: res.status, 
-                error: {isError: true, msg: `Error: ${res.status}! Something went wrong` }})
-            
-        }
-    } catch (error) {
-        setResponse({...response, 
-                    isLoading: false, 
-                    status: error.status,
-                    error: {
-                        isError: true, 
-                        msg: error.msg
-                    } })
-        
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    if (res?.ok) {
+      setResponse({
+        status: res.status,
+        isLoading: false,
+        error: { isError: false, msg: null },
+        data: data,
+      });
+    } else {
+      setResponse({
+        ...response,
+        isLoading: false,
+        status: res.status,
+        error: {
+          isError: true,
+          msg: `Error: ${res.status}! Something went wrong`,
+        },
+      });
     }
-}
+  } catch (error) {
+    setResponse({
+      ...response,
+      isLoading: false,
+      status: error.status,
+      error: {
+        isError: true,
+        msg: error.msg,
+      },
+    });
+  }
+};
+export default useFetch;
