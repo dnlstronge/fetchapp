d7114bff1f17cd0b5a1cbd54cb20a621387c9333
Dan Stronge <dnlstronge@gmail.com>
2023-04-07 14:51:29 +0100

    [x] - format

--- src/Components/Dogs/DogsTwo.js
+++ src/Components/Dogs/DogsTwo.js
@@ -7,40 +7,39 @@ import useFetch from "../../Hooks/useFetch";
 
 const DogsTwo = () => {
   //const [data, setData] = useState();
-  const [data] = useFetch("https://dog.ceo/api/breeds/image/random/3") 
-  const [dogs, setDogs] = useState(false)
-  
+  const [data] = useFetch("https://dog.ceo/api/breeds/image/random/3");
+  const [dogs, setDogs] = useState(false);
 
-useEffect(() => {
-    console.log(data)
-}, [data])
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
-const buttonHandler = () => {
-    setDogs(!dogs)
-}
+  const buttonHandler = () => {
+    setDogs(!dogs);
+  };
 
-
- 
   return (
     <div className={classes.container}>
       <h4 className={classes.heading}>
         Dogs using custom hooks
         <p className={classes.para}>
           I've built this section to try out a custom hook I created to handle
-          simple fetch requests and errors which fall outside of the fetch API.
-          Upon button press It should return some dog images from the previously
-          used free api.
+          simple fetch requests and errors. This time the data is fetched upon
+          render and whether they are displayed is subject to state. The dogs
+          will be the same Dogs when the component is first rendered.
+          <br></br>
+          Upon button press It returns some dog images from the previously used
+          free api and using a custom hook.
         </p>
         <Button onClick={buttonHandler} text="Show me dogs"></Button>
-        {data.isloading && 
-        <p>Loading dogs...</p>}
-        {dogs && data.data.length > 0 &&
-        <section className={classes.sectionImages}>
-          {data.data.map((dog) => {
-            return <img className={classes.images} src={dog} alt="dog" />;
-          })}
-        </section>
-      }
+        {data.isloading && <p>Loading dogs...</p>}
+        {dogs && data.data.length > 0 && (
+          <section className={classes.sectionImages}>
+            {data.data.map((dog) => {
+              return <img className={classes.images} src={dog} alt="dog" />;
+            })}
+          </section>
+        )}
       </h4>
     </div>
   );
