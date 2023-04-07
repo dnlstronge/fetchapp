55cda0c4aff58e9e822b8744f85920800297d287
Dan Stronge <dnlstronge@gmail.com>
2023-04-07 08:29:25 +0100

    [x] - create custom fetch hook

---
+++ src/Hooks/useErrors.js
@@ -0,0 +1,3 @@
+const useErrors = (res) => {
+
+}
--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,5 +1,31 @@
 import useErrorHandler from "./useErrorHandler"
+import React, { useState } from "react"
 
-const useFetch = (url, ) => {
+/* Custom fetch hook. 
 
+2 pieces of state - errors and data
+2 x objects error and data
+
+*/
+
+const useFetch = async(url) => {
+    const [data, setData] = useState()
+    const [error, setError] = useState({
+        isError: false,
+        status: null,
+        msg: null
+    })
+    try {
+        const res = await fetch(url)
+        const data = await res.json()
+        if(res?.ok) {
+            setData(data)
+        } else {
+            setError({isError: true, status: res.status, msg: `Error: ${res.status}! Something went wrong` })
+            
+        }
+    } catch (error) {
+        setError({isError: true, status: error.status, msg: error.message})
+        
+    }
 }
