ed79bf91cb033f0545f6859e784ac8b2de767f19
Dan Stronge <dnlstronge@gmail.com>
2023-04-08 12:00:21 +0100

    [x] - format

--- src/Components/Dogs/DogsBreed.js
+++ src/Components/Dogs/DogsBreed.js
@@ -6,31 +6,31 @@ import React, { useState, useEffect } from "react";
 import classes from "./DogsBreed.module.css";
 import DogSelector from "../../UI/DogSelector";
 import Button from "../../UI/Button";
-import useFetch from "../../Hooks/useFetch"
-
+import useFetch from "../../Hooks/useFetch";
 
 const DogsBreed = () => {
-  
-  const [breedSelect, setBreedSelect] = useState(null)
-  const [showBreed, setShowBreed] = useState(false)
-  
+  const [breedSelect, setBreedSelect] = useState(null);
+  const [showBreed, setShowBreed] = useState(false);
 
-const breedHandler = (e) => {
-    console.log(e.target.value)
-    const breed = e.target.value.trim().toLowerCase().replace(/ /g, "")
-    setBreedSelect(breed)
-}
+  const breedHandler = (e) => {
+    console.log(e.target.value);
+    const breed = e.target.value.trim().toLowerCase().replace(/ /g, "");
+    setBreedSelect(breed);
+  };
 
-const data = useFetch(`https://dog.ceo/api/breed/${breedSelect}/images/random/3`)
-const handleFetch = () => {
-  setShowBreed(!showBreed)
-}
+  const data = useFetch(
+    `https://dog.ceo/api/breed/${breedSelect}/images/random/3`
+  );
+  const handleFetch = () => {
+    setShowBreed(!showBreed);
+  };
 
-  return <div className={classes.container}>
-        <DogSelector selectDog={breedHandler}/> 
-        <Button disabled={false} text="Show" onClick={handleFetch}/>
-
-  </div>;
+  return (
+    <div className={classes.container}>
+      <DogSelector selectDog={breedHandler} />
+      <Button disabled={false} text="Show" onClick={handleFetch} />
+    </div>
+  );
 };
 
 export default DogsBreed;
