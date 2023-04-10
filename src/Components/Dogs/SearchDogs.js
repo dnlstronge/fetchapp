9388129ffd209dec5f4f50b6235e0a5322f80a87
Dan Stronge <dnlstronge@gmail.com>
2023-04-10 10:20:31 +0100

    [x] - add logic [x] - format

--- src/Components/Dogs/SearchDogs.js
+++ src/Components/Dogs/SearchDogs.js
@@ -1,41 +1,54 @@
 import React, { useState } from "react";
-import classes from "./SearchDogs.module.css"
-import Button from "../../UI/Button"
-import useFetch from "../../Hooks/useFetch"
-import Search from "../../Data/Search"
+import classes from "./SearchDogs.module.css";
+import Button from "../../UI/Button";
+import useFetch from "../../Hooks/useFetch";
+import Search from "../../Data/Search";
 
 const dogs = Search;
 
 const SearchDogs = () => {
-    const [isValid, setisValid] = useState(false)
-    const [searchTerm, setSearchTerm] = useState("")
-    const data = useFetch(`https://dog.ceo/api/breed/${searchTerm}/images/random/3`, isValid)
-    const handleSearch = () => {
+  const [showDogs, setShowDogs] = useState(false);
+  const [isValid, setisValid] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const data = useFetch(
+    `https://dog.ceo/api/breed/${searchTerm}/images/random/3`,
+    isValid
+  );
 
+  const handleSearch = () => {
+    if (isValid) {
+      setShowDogs(true);
+    } else {
+      setShowDogs(false);
     }
-    const handleSearchTerm = (e) => {
-        let validate = e.target.value.trim().toLowerCase().replace(/ /g, "")
-        let validated = dogs[validate]
-        if(validated) {
-            console.log(validated)
-        }
-        else {
-            console.log("No entries found")
-        }
-        
-        
+  };
+  const handleSearchTerm = (e) => {
+    let validate = e.target.value.trim().toLowerCase().replace(/ /g, "");
+    let validated = dogs[validate];
+    if (validated) {
+      console.log(validated);
+      setisValid(true);
+      setSearchTerm(validated);
+    } else {
+      console.log("No entries found");
+      setisValid(false);
+      setSearchTerm("");
+      setShowDogs(false);
     }
+  };
 
-    return (
-
-        <div className={classes.container}>
-            <label className={classes.label} htmlFor="searchbar"> 
-                <input  type="text" onChange={handleSearchTerm} className={classes.input}></input>
-                <Button onClick={handleSearch}text="Search" />
-            </label>
-    
-        </div>
-    )
-}
+  return (
+    <div className={classes.container}>
+      <label className={classes.label} htmlFor="searchbar">
+        <input
+          type="text"
+          onChange={handleSearchTerm}
+          className={classes.input}
+        ></input>
+        <Button onClick={handleSearch} text="Search" />
+      </label>
+    </div>
+  );
+};
 
 export default SearchDogs;
