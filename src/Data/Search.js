a06c8b2fe826873fde002690a6e78811df05f9a8
Dan Stronge <dnlstronge@gmail.com>
2023-04-10 12:14:42 +0100

    [X] - alphabetize list

--- src/Data/Search.js
+++ src/Data/Search.js
@@ -1,34 +1,80 @@
-
+/* data search-term validation
+key = searched term (sanitized)
+value = actual value used in url
+*/
 
 
 const Search = {
-    englishbulldog: "bulldog/english",
-    bulldog: "bulldog/english",
-    frenchpoodle: "poodle/french",
+
+    /* A */
+    /* B */
     bordercollie: "collie/border",
     boxer: "boxer",
+    bloodhound: "hound/blood",
     beagle: "beagle",
+    bulldog: "bulldog/english",
+    /* C */
     cockapoo: "cockapoo",
     collie: "collie",
+    /* D */
+
     dalmatian: "dalmatian",
     dachshund: "dachshund",
     dingo: "dingo",
     doberman: "doberman",
     dobermanpinscher: "doberman",
+    /* E */
     englishmastiff: "mastiff/english",
+    englishbulldog: "bulldog/english",
+    /* F */
+    frenchpoodle: "poodle/french",
+    
+    /* G */
+    golden: "retriever/golden",
+    goldenretriever: "retriever/golden",
     germanshepherd: "germanshepherd",
     greatdane: "dane/great",
+    /* H */
+    /* I */
     irishwolfhound: "wolfhound/irish",
-    wolfhound: "wolfhound/irish",
-    mastiff: "mastiff",
+    /* J */
+    /* K */
+    /* L */
     labrador: 'labrador',
     labradorretriever: "labrador",
+    /* M */
+    mastiff: "mastiff",
+    
+    /* N */
+    /* O */
+    /* P */
+    /* Q */
+    /* R */
     retriever: "retriever/golden",
-    goldenretriever: "retriever/golden",
+    /* S */
     shihtzu: "shihtzu",
+    /* T */
+    /* U */
+    /* V */
+    /* W */
+    wolfhound: "wolfhound/irish",
     westie: "terrier/westhighland",
     westhighland: "terrier/westhighland",
     westhighlandterrier: "terrier/westhighland",
+    /* X */
+    /* Y */
+    /* Z */
+    
+  
+    
+    
+    
+
+    
+   
+  
+ 
+
 
 
 
