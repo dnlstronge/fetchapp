2a1deb29a840b8ac9a9091170421c873d69a4930
Dan Stronge <dnlstronge@gmail.com>
2023-04-10 14:29:56 +0100

    [x] - set up new component (Next)

---
+++ src/Components/Next/Next.js
@@ -0,0 +1,16 @@
+import React from "react"
+import classes from "./Next.module.css"
+
+/* new component to try out manipulating data from a different api */
+
+
+
+
+const Next = () => {
+    return (
+        <div className={classes.container}></div>
+    )
+}
+
+
+export default Next;
