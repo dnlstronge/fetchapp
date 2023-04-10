1dc96454a041ce30df2868237478e0f3e9ca1b3f
Dan Stronge <dnlstronge@gmail.com>
2023-04-10 09:54:18 +0100

    [x] - set up useFetch

--- src/Components/Dogs/SearchDogs.js
+++ src/Components/Dogs/SearchDogs.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import classes from "./SearchDogs.module.css"
 import Button from "../../UI/Button"
+import useFetch from "../../Hooks/useFetch"
 
 const SearchDogs = () => {
-
+    const [isValid, setisValid] = useState(false)
     const [searchTerm, setSearchTerm] = useState("")
+    const data = useFetch(`https://dog.ceo/api/breed/${breedSelect}/images/random/3`, isValid)
     const handleSearch = (e) => {
 
     }
