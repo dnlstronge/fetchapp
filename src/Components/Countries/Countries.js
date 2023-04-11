900f53653b6ad0229a093f363575060f23f8a5e2
Dan Stronge <dnlstronge@gmail.com>
2023-04-11 09:05:10 +0100

    [x] - add css

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -1,7 +1,9 @@
 import React from "react"
 import classes from "./Countries.module.css"
+import SearchBar from "../../UI/Searchbar"
 
 /* new component to try out manipulating data from a different api */
+/* Build custom searchbar component and import */
 
 
 
@@ -9,9 +11,10 @@ import classes from "./Countries.module.css"
 const Countries = () => {
     return (
         <div className={classes.container}>
-            <h4>Countries API</h4>
-            <p>This section uses the countries api, it allows users to search
+            <h4 className={classes.heading}>Countries API</h4>
+            <p className={classes.para}>This section uses the countries api, it allows users to search
             for a particular country and display data</p>
+            <SearchBar />
         </div>
     )
 }
