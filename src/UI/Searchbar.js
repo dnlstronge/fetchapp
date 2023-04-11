f7e8b0dcbabc50992986080f089e1e087d0e8467
Dan Stronge <dnlstronge@gmail.com>
2023-04-11 08:50:20 +0100

    [x] - create new component - search bar

---
+++ src/UI/Searchbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./SearchBar.module.csss"
+
+const SearchBar = () => {
+    return (
+        <div className={classes.container}></div>
+    )
+}
+
+export default SearchBar;
