fdee6363dee4257244268de59e1e100b4f51ac1b
Dan Stronge <dnlstronge@gmail.com>
2023-04-11 08:52:08 +0100

    [x] - add template

--- src/UI/Searchbar.js
+++ src/UI/Searchbar.js
@@ -3,7 +3,11 @@ import classes from "./SearchBar.module.csss"
 
 const SearchBar = () => {
     return (
-        <div className={classes.container}></div>
+        <div className={classes.container}>
+            <label htmlFor="search"></label>
+            <input id="search"></input>
+            <button></button>
+        </div>
     )
 }
 
