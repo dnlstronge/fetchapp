c3584511fcc821a8e063241742fded234c150e34
Dan Stronge <dnlstronge@gmail.com>
2023-04-12 10:30:46 +0100

    [x] - add css

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -9,6 +9,7 @@ import Card from "./Card";
 /*API = https://restcountries.com/v3.1/name/{name} */
 
 const Countries = () => {
+  const [loading, setLoading] = useState(false)
   const [search, setSearch] = useState("");
   const [showData, setShowData] = useState(false);
   const [error, setError] = useState({
@@ -20,23 +21,28 @@ const Countries = () => {
   useEffect(() => {
     setError({isError: false, msg: null})
     if (search.length > 0) {
+      setLoading(true)
       try {
         fetch(`https://restcountries.com/v3.1/name/${search}`).then((res) => {
           console.log(search);
           console.log(res);
           if (res.ok) {
+            
             res.json().then((items) => {
               setData(items);
               setShowData(true);
+              setLoading(false)
             });
           } else {
+            setLoading(false)
             setShowData(false);
-            setError({ isError: true, msg: res.status });
+            setError({ isError: true, msg: "No results found" });
           }
         });
 
         
       } catch (error) {
+        setLoading(false)
         setError({ isError: true, msg: error.msg });
         setShowData(false);
       }
@@ -48,12 +54,15 @@ const Countries = () => {
       <h4 className={classes.heading}>Countries API</h4>
       <p className={classes.para}>
         This section uses the countries api, it allows users to search for a
-        particular country and display data
+        particular country and display data which is specific to each. The search also returns 
+        the specific coat of arms, flag and some general data about the country. 
       </p>
       <SearchBar onClick={setSearch} />
       {error.isError && search.length > 0 && (
         <p className={classes.error}>Ooops... {error.msg}</p>
       )}
+      {loading && 
+        <p className={classes.error}>Loading...</p>}
 
       {showData && data && !error.isError && (
         <Card
